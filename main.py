d5287bcfc6e6161a6ce03ff866c95319c57c3b8a
George <george9722@gmail.com>
2019-02-22 21:52:43 -0500

    Added tutorial content from website

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
-import tkinter
+import tkinter as tk
 
-m = tkinter.Tk()
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack()
+        self.create_widgets()
 
-m.mainloop()
+    def create_widgets(self):
+        self.hi_there = tk.Button(self)
+        self.hi_there["text"] = "Hello World\n(click me)"
+        self.hi_there["command"] = self.say_hi
+        self.hi_there.pack(side="top")
+
+        self.quit = tk.Button(self, text="QUIT", fg="red",
+                              command=self.master.destroy)
+        self.quit.pack(side="bottom")
+
+    def say_hi(self):
+        print("hi there, everyone!")
+
+mainWindow = tk.Tk()
+
+app = Application(master = mainWindow)
+app.mainloop()
