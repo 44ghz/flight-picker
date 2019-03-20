d938132879a75df708ff5611f59b159274353fd4
George <george9722@gmail.com>
2019-03-19 20:56:13 -0400

    Moved many functions to other modules, added categorization of flights

--- modules/results.py
+++ modules/results.py
@@ -1,95 +1,69 @@
 import tkinter as tk # Main GUI capabilities
-from tkinter import ttk # Various other tkinter widgets (mainly combobox)
-from tkinter import Scrollbar
 from modules import dataretrieval as dr
+from modules import options as op
+from modules import tkfunctions as tf
 
-def convert_df(df):
-    COLUMNS = 12 # Number of relevant columns
-    # AIRCRAFT_COLUMN = 8
-    #
-    # aircraftData = dr.open_data('AircraftNames.csv')
-    # aircraftList = dr.get_data(df, AIRCRAFT_COLUMN)
-    # aircraftList.pop(0)
-    # print(aircraftList)
-    # aircraftNames = dr.get_aircraft_names(aircraftData, aircraftList)
-    #
-    # aircraftDict = {}
-    #
-    # currAircraft = 0
-    # for aircraft in aircraftList:
-    #     aircraftDict[aircraft] = aircraftNames[currAircraft]
-    #     currAircraft += 1
+def find_flights(mode, frame, flightData):
+    if(mode.get() == 1):
+        automatic(frame, flightData)
+    else:
+        manual(frame, flightData)
 
-    monthDict = {}
-    monthDict[1] = "January"
-    monthDict[2] = "February"
-    monthDict[3] = "March"
-    monthDict[4] = "April"
-    monthDict[5] = "May"
-    monthDict[6] = "June"
-    monthDict[7] = "July"
-    monthDict[8] = "August"
-    monthDict[9] = "September"
-    monthDict[10] = "October"
-    monthDict[11] = "November"
-    monthDict[12] = "December"
+def automatic(frame, flightData):
+    DIST_COLUMN = 5 # The columns that correspond to the flight dataframe
+    CARRIER_COLUMN = 6
+    ORIGIN_COLUMN = 7
+    DEST_COLUMN = 8
+    AIRCRAFT_COLUMN = 9
+    OR_STATE_COLUMN = 10
+    DEST_STATE_COLUMN = 11
+    MONTH_COLUMN = 12
 
-    flights = [] # The list of lists containing information about every flight
-    rows = len(df.index) # The number of rows in the DataFrame
+    flightsForDistance = {} # Use predetermined ranges
+    flightsForCarrier = {}
+    flightsForOriginCity = {}
+    flightsForDestCity = {}
+    flightsForAircraft = {}
+    flightsForOrState = {}
+    flightsForDestState = {}
+    flightsForMonth = {}
 
-    for row in range(rows):
-        currentFlight = [] # Reset the current row
-        currentFlight.append("")
-        for column in range(COLUMNS):
-            currentFlight.append(df.iloc[row, column]) # Add the current cell to the current flight list
-        #currentFlight[AIRCRAFT_COLUMN] =
-        currentFlight[COLUMNS] = monthDict[currentFlight[COLUMNS]] # Replace the month number with the month name
-        flights.append(currentFlight) # Add the new flight list to the list of flights
+    flightList = dr.convert_df(flightData)
+    optionsList = op.get_options_lists(flightData)
 
-    return flights
+    # Taking each unique option from the options lists and creating the key values for each dictionary
+    for carrier in optionsList[1]:
+        flightsForCarrier[carrier] = [] # Each value for the keys will be a list, which will then have lists inside (aka flights)
 
-def create_results(frame, flights):
-    flightTree = ttk.Treeview(frame)
-    flightTree.config(height = 33)
-    flightTree.pack(side = 'left')
+    for origin in optionsList[2]:
+        flightsForOriginCity[origin] = []
 
-    treeScrollBar = ttk.Scrollbar(frame,
-        orient = "vertical",
-        command = flightTree.yview)
-    treeScrollBar.pack(side = 'right', fill = 'y')
+    for destination in optionsList[3]:
+        flightsForDestCity[destination] = []
 
-    flightTree.configure(yscrollcommand= treeScrollBar.set)
+    for aircraft in optionsList[4]:
+        flightsForAircraft[aircraft] = []
 
-    flightTree["columns"] = ("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13")
-    flightTree['show'] = 'headings'
+    for originState in optionsList[5]:
+        flightsForOrState[originState] = []
 
-    flightTree.column("1", width = 110, anchor = 'c')
-    flightTree.column("2", width = 110, anchor = 'c')
-    flightTree.column("3", width = 110, anchor = 'c')
-    flightTree.column("4", width = 110, anchor = 'c')
-    flightTree.column("5", width = 110, anchor = 'c')
-    flightTree.column("6", width = 110, anchor = 'c')
-    flightTree.column("7", width = 110, anchor = 'c')
-    flightTree.column("8", width = 110, anchor = 'c')
-    flightTree.column("9", width = 110, anchor = 'c')
-    flightTree.column("10", width = 110, anchor = 'c')
-    flightTree.column("11", width = 110, anchor = 'c')
-    flightTree.column("12", width = 110, anchor = 'c')
-    flightTree.column("13", width = 110, anchor = 'c')
+    for destState in optionsList[6]:
+        flightsForDestState[destState] = []
 
-    flightTree.heading("1", text = "% of Success")
-    flightTree.heading("2", text = "Scheduled")
-    flightTree.heading("3", text = "Performed")
-    flightTree.heading("4", text = "Seats")
-    flightTree.heading("5", text = "Passengers")
-    flightTree.heading("6", text = "Distance (mi.)")
-    flightTree.heading("7", text = "Carrier")
-    flightTree.heading("8", text = "Origin")
-    flightTree.heading("9", text = "Destination")
-    flightTree.heading("10", text = "Aircraft")
-    flightTree.heading("11", text = "Origin State")
-    flightTree.heading("12", text = "Dest State")
-    flightTree.heading("13", text = "Month")
+    for month in optionsList[7]:
+        flightsForMonth[month] = []
 
-    for flight in range(len(flights)):
-        flightTree.insert("", 'end', values = flights[flight])
+    for currFlight in range(len(flightList)): # For every flight in the list, categorize it to each dict
+        currentFlight = flightList[currFlight]
+        flightsForCarrier[flightList[currFlight][CARRIER_COLUMN]].append(currentFlight)
+        flightsForOriginCity[currentFlight[ORIGIN_COLUMN]].append(currentFlight)
+        flightsForDestCity[currentFlight[DEST_COLUMN]].append(currentFlight)
+        flightsForAircraft[currentFlight[AIRCRAFT_COLUMN]].append(currentFlight)
+        flightsForOrState[currentFlight[OR_STATE_COLUMN]].append(currentFlight)
+        flightsForDestState[currentFlight[DEST_STATE_COLUMN]].append(currentFlight)
+        flightsForMonth[currentFlight[MONTH_COLUMN]].append(currentFlight)
+
+    tf.create_auto_tabs(frame)
+
+def manual(frame, flightData):
+    tf.create_manual_results_panel(frame, flightData)
